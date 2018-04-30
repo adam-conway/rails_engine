1849af0289af0d634825938f3e318f24500e25ec
agpiermarini <piermarini.andrew@gmail.com>
2018-04-30 17:32:25 -0600

    Update factories with missing attributes

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::StarWars.character }
     description { Faker::Fallout.quote }
-    unit_price [123,345,456].sample
+    price [123,345,456].sample
     merchant
   end
 end
