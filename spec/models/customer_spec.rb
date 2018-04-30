d0c92a3e5fddf252ae19d2dc1b8a6f242467ad65
adam-conway <adam.n.conway@gmail.com>
2018-04-30 15:49:41 -0600

    Added in validation tests for customers, merchants, and transactions

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,33 @@ RSpec.describe Customer, type: :model do
     it {should validate_presence_of(:first_name)}
     it {should validate_presence_of(:last_name)}
   end
+
+  describe 'Field validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a first name' do
+        customer = Customer.create(
+          last_name: "Conway"
+        )
+
+        expect(customer).to_not be_valid
+      end
+      it 'is invalid without a last name' do
+        customer = Customer.create(
+          first_name: "Adam"
+        )
+
+        expect(customer).to_not be_valid
+      end
+    end
+    context 'valid attributes' do
+      it 'is valid with all attributesß' do
+        customer = Customer.create(
+          first_name: "Adam",
+          last_name: "Conway"
+        )
+
+        expect(customer).to be_valid
+      end
+    end
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Merchant, type: :model do
   describe "validations" do
     it {should validate_presence_of(:name)}
   end
+
+  describe 'Field validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a name' do
+        merchant = Merchant.create()
+
+        expect(merchant).to_not be_valid
+      end
+    end
+    context 'valid attributes' do
+      it 'is valid with all attributesß' do
+        merchant = Merchant.create(
+          name: "Adam"
+        )
+
+        expect(merchant).to be_valid
+      end
+    end
+  end
 end
