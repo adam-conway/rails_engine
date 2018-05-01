1f7b40a6461c2f68f0f9d773ad0e78c9b000cfb0
adam-conway <adam.n.conway@gmail.com>
2018-05-01 00:02:56 -0600

    Finished query for a merchants total revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,8 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   has_many :invoices
+
+  def single_merchant_revenue
+    invoices.where(status: "Success").joins(:invoice_items).sum(:price)
+  end
 end
