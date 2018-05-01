fea4648e495f71cb9583af11d0bf041d194870f6
adam-conway <adam.n.conway@gmail.com>
2018-04-30 23:20:57 -0600

    Working on searching for merchant based on params

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
       resources :merchants, only: [:index, :show]
     end
   end
+
+  # get "/api/v1/merchants/find", to: 'search#find'
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe "Merchants API" do
+
   it "sends a list of merchants" do
     merchant_list = create_list(:merchant, 3)
 
@@ -12,6 +13,7 @@ describe "Merchants API" do
 
     expect(merchants.count).to eq(merchant_list.count)
   end
+
   it "can get one merchant by its id" do
     id = create(:merchant).id
 
@@ -22,4 +24,62 @@ describe "Merchants API" do
     expect(response).to be_success
     expect(merchant["id"]).to eq(id)
   end
+
+  it "can get one merchant based on id" do
+    merchant_list = create_list(:merchant, 10)
+
+    get "/api/v1/merchants/find?id=#{merchant_list[1].id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(merchant_list[1].id)
+  end
+
+  it "can get one merchant based on name" do
+    merchant_list = create_list(:merchant, 10)
+
+    get "/api/v1/merchants/find?name=#{merchant_list[3].name}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(merchant_list[3].id)
+  end
+
+  it "can get one merchant based on name (case-insensitive)" do
+    merchant_list = create_list(:merchant, 10)
+
+    get "/api/v1/merchants/find?name=#{merchant_list[3].name}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(merchant_list[3].id)
+  end
+
+  it "can get one merchant based on created_at" do
+    skip
+    merchant_list = create_list(:merchant, 10)
+
+    get "/api/v1/merchants/find?created_at=#{merchant_list[5].created_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(merchant_list[5].id)
+  end
+
+  it "can get one merchant based on updated_at" do
+    skip
+    merchant_list = create_list(:merchant, 10)
+
+    get "/api/v1/merchants/find?updated_at=#{merchant_list[7].updated_at}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(merchant_list[7].id)
+  end
+
 end
