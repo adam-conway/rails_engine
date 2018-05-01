f0e2d7c78cc3f1fafdb0d32cf57c77a0e4d623fa
adam-conway <adam.n.conway@gmail.com>
2018-05-01 17:07:26 -0600

    Finished almost everything for invoices relationships. Waiting on serializers

--- spec/requests/api/v1/relationships/invoices_spec.rb
+++ spec/requests/api/v1/relationships/invoices_spec.rb
@@ -60,5 +60,48 @@ describe 'Invoices API' do
       expect(response).to be_success
       expect(invoice_list[7].items.count).to eq(items.count)
     end
+
+    it 'can return customer associated with an invoice' do
+      customer_list = create_list(:customer, 3)
+      # merchant_list = create_list(:merchant, 10)
+      # item_list = create_list(:item, 10)
+      5.times do
+        create(:invoice, customer: customer_list.sample)
+      end
+      # 40.times do
+      #   create(:transaction, invoice: invoice_list.sample)
+      # end
+      # 50.times do
+      #   create(:invoice_item, invoice: invoice_list.sample, item: item_list.sample)
+      # end
+
+      get "/api/v1/invoices/#{Invoice.first.id}/customer"
+
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(Invoice.first.customer.id).to eq(customer["id"])
+    end
+
+    it 'can return merchant associated with an invoice' do
+      merchant_list = create_list(:merchant, 3)
+      # item_list = create_list(:item, 10)
+      5.times do
+        create(:invoice, merchant: merchant_list.sample)
+      end
+      # 40.times do
+      #   create(:transaction, invoice: invoice_list.sample)
+      # end
+      # 50.times do
+      #   create(:invoice_item, invoice: invoice_list.sample, item: item_list.sample)
+      # end
+
+      get "/api/v1/invoices/#{Invoice.first.id}/merchant"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(Invoice.first.merchant.id).to eq(merchant["id"])
+    end
   end
 end
