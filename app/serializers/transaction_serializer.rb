c922c7fdd9996852ce9a20751a84ebe1416c0216
adam-conway <adam.n.conway@gmail.com>
2018-05-01 14:04:11 -0600

    Finished everything on transactions

---
+++ app/serializers/transaction_serializer.rb
@@ -0,0 +1,7 @@
+class TransactionSerializer < ActiveModel::Serializer
+  attributes :credit_card_number, :id, :invoice_id, :result
+
+  def credit_card_number
+    object.credit_card_number.to_s
+  end
+end
