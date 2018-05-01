4850438fa67f7a9d1c78b0f0757ff12e96fff9b7
adam-conway <adam.n.conway@gmail.com>
2018-05-01 11:46:49 -0600

    Working on searching case-insensitive

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,12 +1,17 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
   def show
-    # binding.pry
     render json: Merchant.find_by(search_params)
+  end
+
+  def index
+    binding.pry
+    render json: Merchant.where(search_params)
   end
 
   private
     def search_params
+      params.each {|k,v| v.downcase!}
       params.permit(:id, :name, :created_at, :updated_at)
     end
 end
--- spec/requests/api/v1/records/merchants_records_request_spec.rb
+++ spec/requests/api/v1/records/merchants_records_request_spec.rb
@@ -24,67 +24,139 @@ describe "Merchants API" do
     expect(response).to be_success
     expect(merchant["id"]).to eq(id)
   end
+  describe "Single finders" do
+    it "can get one merchant based on id" do
+      merchant_list = create_list(:merchant, 10)
 
-  it "can get one merchant based on id" do
-    merchant_list = create_list(:merchant, 10)
+      get "/api/v1/merchants/find?id=#{merchant_list[1].id}"
 
-    get "/api/v1/merchants/find?id=#{merchant_list[1].id}"
+      merchant = JSON.parse(response.body)
 
-    merchant = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(merchant["id"]).to eq(merchant_list[1].id)
+    end
 
-    expect(response).to be_success
-    expect(merchant["id"]).to eq(merchant_list[1].id)
+    it "can get one merchant based on name" do
+      merchant_list = create_list(:merchant, 10)
+
+      get "/api/v1/merchants/find?name=#{merchant_list[3].name}"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant["id"]).to eq(merchant_list[3].id)
+    end
+
+    it "can get one merchant based on name (case-insensitive)" do
+      merchant_list = create_list(:merchant, 10)
+
+      get "/api/v1/merchants/find?name=#{merchant_list[3].name.upcase}"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant["id"]).to eq(merchant_list[3].id)
+    end
+
+    it "can get one merchant based on created_at" do
+      date = "2018-04-30 10:45:00 UTC"
+      merchant1 = create(:merchant, created_at: "2018-04-30 10:45:00 UTC")
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+
+      get "/api/v1/merchants/find?created_at=#{merchant1.created_at}"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant["id"]).to eq(merchant1.id)
+    end
+
+    it "can get one merchant based on updated_at" do
+      date = "2018-04-30 10:45:00 UTC"
+      merchant1 = create(:merchant, updated_at: date)
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+
+      get "/api/v1/merchants/find?updated_at=#{merchant1.updated_at}"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant["id"]).to eq(merchant1.id)
+    end
   end
+  describe "Multi finders" do
+    it "can get all merchants based on id" do
+      merchant_list = create_list(:merchant, 10)
+      search_param = 1
+      factory_merchants = merchant_list.find_all do |merchant|
+        merchant.id == search_param
+      end
 
-  it "can get one merchant based on name" do
-    merchant_list = create_list(:merchant, 10)
+      get "/api/v1/merchants/find_all?id=#{search_param}"
 
-    get "/api/v1/merchants/find?name=#{merchant_list[3].name}"
+      response_merchants = JSON.parse(response.body)
 
-    merchant = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(factory_merchants).to eq(response_merchants)
+    end
 
-    expect(response).to be_success
-    expect(merchant["id"]).to eq(merchant_list[3].id)
+    it "can get all merchants based on name" do
+      merchant_list = create_list(:merchant, 10)
+      merchant_list << create(:merchant, name: merchant_list[0].name)
+
+      factory_merchants = merchant_list.find_all do |merchant|
+        merchant.name == merchant_list[0].name
+      end
+
+      get "/api/v1/merchants/find_all?name=#{merchant_list[0].name}"
+
+      response_merchants = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(factory_merchants).to eq(response_merchants)
+    end
+
+    #
+    # it "can get one merchant based on name (case-insensitive)" do
+    #   skip
+    #   merchant_list = create_list(:merchant, 10)
+    #
+    #   get "/api/v1/merchants/find?name=#{merchant_list[3].name.upcase}"
+    #
+    #   merchant = JSON.parse(response.body)
+    #
+    #   expect(response).to be_success
+    #   expect(merchant["id"]).to eq(merchant_list[3].id)
+    # end
+    #
+    # it "can get one merchant based on created_at" do
+    #   date = "2018-04-30 10:45:00 UTC"
+    #   merchant1 = create(:merchant, created_at: "2018-04-30 10:45:00 UTC")
+    #   merchant2 = create(:merchant)
+    #   merchant3 = create(:merchant)
+    #
+    #   get "/api/v1/merchants/find?created_at=#{merchant1.created_at}"
+    #
+    #   merchant = JSON.parse(response.body)
+    #
+    #   expect(response).to be_success
+    #   expect(merchant["id"]).to eq(merchant1.id)
+    # end
+    #
+    # it "can get one merchant based on updated_at" do
+    #   date = "2018-04-30 10:45:00 UTC"
+    #   merchant1 = create(:merchant, updated_at: date)
+    #   merchant2 = create(:merchant)
+    #   merchant3 = create(:merchant)
+    #
+    #   get "/api/v1/merchants/find?updated_at=#{merchant1.updated_at}"
+    #
+    #   merchant = JSON.parse(response.body)
+    #
+    #   expect(response).to be_success
+    #   expect(merchant["id"]).to eq(merchant1.id)
+    # end
   end
-
-  it "can get one merchant based on name (case-insensitive)" do
-    skip
-    merchant_list = create_list(:merchant, 10)
-
-    get "/api/v1/merchants/find?name=#{merchant_list[3].name.upcase}"
-
-    merchant = JSON.parse(response.body)
-
-    expect(response).to be_success
-    expect(merchant["id"]).to eq(merchant_list[3].id)
-  end
-
-  it "can get one merchant based on created_at" do
-    date = "2018-04-30 10:45:00 UTC"
-    merchant1 = create(:merchant, created_at: "2018-04-30 10:45:00 UTC")
-    merchant2 = create(:merchant)
-    merchant3 = create(:merchant)
-
-    get "/api/v1/merchants/find?created_at=#{merchant1.created_at}"
-
-    merchant = JSON.parse(response.body)
-
-    expect(response).to be_success
-    expect(merchant["id"]).to eq(merchant1.id)
-  end
-
-  it "can get one merchant based on updated_at" do
-    date = "2018-04-30 10:45:00 UTC"
-    merchant1 = create(:merchant, updated_at: date)
-    merchant2 = create(:merchant)
-    merchant3 = create(:merchant)
-
-    get "/api/v1/merchants/find?updated_at=#{merchant1.updated_at}"
-
-    merchant = JSON.parse(response.body)
-
-    expect(response).to be_success
-    expect(merchant["id"]).to eq(merchant1.id)
-  end
-
 end
