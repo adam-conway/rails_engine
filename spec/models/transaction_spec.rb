bb86c8a5cb843baab7bc51cfc901a766b309ec06
agpiermarini <piermarini.andrew@gmail.com>
2018-04-30 18:12:10 -0600

    Revise import.rake, change transaction column type, remove transaction expiration validation

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe "validations" do
     it {should validate_presence_of(:credit_card_number)}
-    it {should validate_presence_of(:credit_card_expiration_date)}
     it {should validate_presence_of(:result)}
     it {should belong_to(:invoice)}
   end
@@ -13,14 +12,6 @@ RSpec.describe Transaction, type: :model do
       it 'is invalid without a number' do
         transaction = Transaction.create(
           credit_card_expiration_date: 2343,
-          result: "failed"
-        )
-
-        expect(transaction).to_not be_valid
-      end
-      it 'is invalid without an expiration' do
-        transaction = Transaction.create(
-          credit_card_number: 48585695,
           result: "failed"
         )
 
