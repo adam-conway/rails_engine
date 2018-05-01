4623dffec7e428ae0a440cf7a422fb41b150654a
adam-conway <adam.n.conway@gmail.com>
2018-05-01 17:58:12 -0600

    Finished tests for invoice_items_spec

---
+++ spec/requests/api/v1/relationships/invoice_items_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Invoice items API' do
+  describe 'relationships' do
+    it 'can return invoice associated with an invoice_item' do
+      invoice_list = create_list(:invoice, 6)
+      item_list = create_list(:item, 6)
+      invoice_item = create(:invoice_item, item: item_list.sample, invoice: invoice_list.sample)
+
+      get "/api/v1/invoice_items/#{invoice_item.id}/invoice"
+
+      invoice = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoice).to eq(invoice_item.invoice)
+    end
+    it 'can return item associated with an invoice_item' do
+      invoice_list = create_list(:invoice, 6)
+      item_list = create_list(:item, 6)
+      invoice_item = create(:invoice_item, item: item_list.sample, invoice: invoice_list.sample)
+
+      get "/api/v1/invoice_items/#{invoice_item.id}/item"
+
+      invoice_item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item).to eq(invoice_item.item)
+    end
+  end
+end
