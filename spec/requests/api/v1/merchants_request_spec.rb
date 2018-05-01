52704731ecb7cb44a4297c8afd57f4256b62d79f
adam-conway <adam.n.conway@gmail.com>
2018-04-30 21:34:44 -0600

    Finished show portion of merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 describe "Merchants API" do
   it "sends a list of merchants" do
-    create_list(:merchant, 3)
+    merchant_list = create_list(:merchant, 3)
 
     get '/api/v1/merchants'
 
     expect(response).to be_success
 
     merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(merchant_list.count)
+  end
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(id)
   end
 end
