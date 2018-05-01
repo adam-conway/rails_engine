c3398c38dcbf134072ee99a9362e917f1775c418
adam-conway <adam.n.conway@gmail.com>
2018-05-01 17:09:21 -0600

    Test cleanup

--- spec/requests/api/v1/relationships/invoices_spec.rb
+++ spec/requests/api/v1/relationships/invoices_spec.rb
@@ -3,15 +3,10 @@ require 'rails_helper'
 describe 'Invoices API' do
   describe 'relationships' do
     it 'can return transactions associated with an invoice' do
-      # customer_list = create_list(:customer, 10)
-      # merchant_list = create_list(:merchant, 10)
-      # item_list = create_list(:item, 10)
       invoice_list = create_list(:invoice, 10)
       40.times do
         create(:transaction, invoice: invoice_list.sample)
       end
-
-      # invoice_items_list = create_list(:invoice_item, 100)
 
       get "/api/v1/invoices/#{invoice_list[2].id}/transactions"
 
@@ -22,13 +17,7 @@ describe 'Invoices API' do
     end
 
     it 'can return invoice_items associated with an invoice' do
-      # customer_list = create_list(:customer, 10)
-      # merchant_list = create_list(:merchant, 10)
-      # item_list = create_list(:item, 10)
       invoice_list = create_list(:invoice, 10)
-      # 40.times do
-      #   create(:transaction, invoice: invoice_list.sample)
-      # end
       100.times do
         create(:invoice_item, invoice: invoice_list.sample)
       end
@@ -42,13 +31,8 @@ describe 'Invoices API' do
     end
 
     it 'can return items associated with an invoice' do
-      # customer_list = create_list(:customer, 10)
-      # merchant_list = create_list(:merchant, 10)
       item_list = create_list(:item, 10)
       invoice_list = create_list(:invoice, 10)
-      # 40.times do
-      #   create(:transaction, invoice: invoice_list.sample)
-      # end
       50.times do
         create(:invoice_item, invoice: invoice_list.sample, item: item_list.sample)
       end
@@ -63,17 +47,9 @@ describe 'Invoices API' do
 
     it 'can return customer associated with an invoice' do
       customer_list = create_list(:customer, 3)
-      # merchant_list = create_list(:merchant, 10)
-      # item_list = create_list(:item, 10)
       5.times do
         create(:invoice, customer: customer_list.sample)
       end
-      # 40.times do
-      #   create(:transaction, invoice: invoice_list.sample)
-      # end
-      # 50.times do
-      #   create(:invoice_item, invoice: invoice_list.sample, item: item_list.sample)
-      # end
 
       get "/api/v1/invoices/#{Invoice.first.id}/customer"
 
@@ -85,17 +61,10 @@ describe 'Invoices API' do
 
     it 'can return merchant associated with an invoice' do
       merchant_list = create_list(:merchant, 3)
-      # item_list = create_list(:item, 10)
       5.times do
         create(:invoice, merchant: merchant_list.sample)
       end
-      # 40.times do
-      #   create(:transaction, invoice: invoice_list.sample)
-      # end
-      # 50.times do
-      #   create(:invoice_item, invoice: invoice_list.sample, item: item_list.sample)
-      # end
-
+      
       get "/api/v1/invoices/#{Invoice.first.id}/merchant"
 
       merchant = JSON.parse(response.body)
