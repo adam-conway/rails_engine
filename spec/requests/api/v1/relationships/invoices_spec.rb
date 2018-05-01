f8404760292131a02bfea0f543ccf2db1c3b558a
adam-conway <adam.n.conway@gmail.com>
2018-05-01 16:56:29 -0600

    Finished test and endpoint for an invoice's invoice items

--- spec/requests/api/v1/relationships/invoices_spec.rb
+++ spec/requests/api/v1/relationships/invoices_spec.rb
@@ -20,5 +20,25 @@ describe 'Invoices API' do
       expect(response).to be_success
       expect(invoice_list[2].transactions.count).to eq(transactions.count)
     end
+
+    it 'can return invoice_items associated with an invoice' do
+      # customer_list = create_list(:customer, 10)
+      # merchant_list = create_list(:merchant, 10)
+      # item_list = create_list(:item, 10)
+      invoice_list = create_list(:invoice, 10)
+      # 40.times do
+      #   create(:transaction, invoice: invoice_list.sample)
+      # end
+      100.times do
+        create(:invoice_item, invoice: invoice_list.sample)
+      end
+
+      get "/api/v1/invoices/#{invoice_list[4].id}/invoice_items"
+
+      invoice_items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoice_list[4].invoice_items.count).to eq(invoice_items.count)
+    end
   end
 end
