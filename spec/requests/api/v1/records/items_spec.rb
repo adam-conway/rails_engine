fb3845810e633f2cb6770bacfad89bba32c1f072
agpiermarini <piermarini.andrew@gmail.com>
2018-05-01 15:18:36 -0600

    Add find by merchant_id, description, created_at and updated_at

--- spec/requests/api/v1/records/items_spec.rb
+++ spec/requests/api/v1/records/items_spec.rb
@@ -36,85 +36,73 @@ describe 'Invoices API' do
     expect(item["id"]).to eq(id)
   end
 
-  # it 'can return single invoice by passing status param' do
-  #   create(:invoice)
-  #   status = Invoice.last.status
-  #
-  #   get "/api/v1/invoices/find?status=#{status}"
-  #
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice["status"]).to eq(status)
-  # end
-  #
-  # it 'can return single invoice by passing status param; case-insensitive' do
-  #   create(:invoice)
-  #   status = Invoice.last.status
-  #
-  #   get "/api/v1/invoices/find?status=#{status.upcase}"
-  #
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice["status"]).to eq(status)
-  # end
-  #
-  # it 'can return single invoice by passing merchant_id param' do
-  #   create(:invoice)
-  #   merchant_id = Invoice.last.merchant_id
-  #
-  #   get "/api/v1/invoices/find?merchant_id=#{merchant_id}"
-  #
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice["merchant_id"]).to eq(merchant_id)
-  # end
-  #
-  # it 'can return single invoice by passing customer_id param' do
-  #   create(:invoice)
-  #   customer_id = Invoice.last.customer_id
-  #
-  #   get "/api/v1/invoices/find?customer_id=#{customer_id}"
-  #
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice["customer_id"]).to eq(customer_id)
-  # end
-  #
-  # it 'can return single invoice by passing created_at param' do
-  #   create(:invoice)
-  #   id = Invoice.last.id
-  #   created_at = Invoice.last.created_at
-  #
-  #   get "/api/v1/invoices/find?created_at=#{created_at}"
-  #
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice["id"]).to eq(id)
-  # end
-  #
-  # it 'can return single invoice by passing updated_at param' do
-  #   create(:invoice)
-  #   id = Invoice.last.id
-  #   updated_at = Invoice.last.updated_at
-  #
-  #   get "/api/v1/invoices/find?updated_at=#{updated_at}"
-  #
-  #   invoice = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(invoice["id"]).to eq(id)
-  # end
-  #
+  it 'can return single item by passing description param' do
+    create(:item)
+    description = Item.last.description
+
+    get "/api/v1/items/find?description=#{description}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["description"]).to eq(description)
+  end
+
+  skip 'can return single item by passing price param' do
+    create(:item)
+    price = Item.last.price
+
+    get "/api/v1/items/find?price=#{price}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["price"]).to eq(price)
+  end
+
+  it 'can return single item by passing merchant_id param' do
+    create(:item)
+    merchant_id = Item.last.merchant_id
+
+    get "/api/v1/items/find?merchant_id=#{merchant_id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["merchant_id"]).to eq(merchant_id)
+  end
+
+  it 'can return single item by passing created_at param' do
+    create(:item)
+    id = Item.last.id
+    created_at = Item.last.created_at
+
+    get "/api/v1/items/find?created_at=#{created_at}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["id"]).to eq(id)
+  end
+
+  it 'can return single item by passing updated_at param' do
+    create(:item)
+    id = Item.last.id
+    updated_at = Item.last.updated_at
+
+    get "/api/v1/items/find?updated_at=#{updated_at}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["id"]).to eq(id)
+  end
+
   # it 'can return all invoices matching a status param' do
   #   create(:invoice, status: "shipped")
   #   create_list(:invoice, 3, status: "pending")
   #
-  #   status = Invoice.last.status
+  #   status = Item.last.status
   #
   #   get "/api/v1/invoices/find_all?status=#{status}"
   #
