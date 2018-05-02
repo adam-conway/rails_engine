4e3c55f8a6a85c9af733e1e7de4a060c2f0f9cbb
agpiermarini <piermarini.andrew@gmail.com>
2018-05-01 22:53:01 -0600

    Remove pry

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Customers::TransactionsController < ApplicationController
   def index
-    require 'pry'; binding.pry
     render json: Customer.find(params[:customer_id]).transactions
   end
 end
